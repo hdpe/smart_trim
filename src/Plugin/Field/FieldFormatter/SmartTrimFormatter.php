e4992c0b56a3f03f7cafda9ba68a038a4b0b680f
Mark Casias <mark.casias@mediacurrent.com>
2014-10-19 15:09:22 -0400

    Issue #2355687 by krisahil | kbasarab: Fixed Format setting not appearing.

--- src/Plugin/Field/FieldFormatter/SmartTrimFormatter.php
+++ src/Plugin/Field/FieldFormatter/SmartTrimFormatter.php
@@ -9,6 +9,7 @@ namespace Drupal\smart_trim\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'smart_trim' formatter.
@@ -37,16 +38,29 @@ class SmartTrimFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(array $form, array &$form_state) {
-    $element = parent::settingsForm($form, $form_state);
+  public static function defaultSettings() {
+    return array(
+      'trim_length' => '600',
+      'trim_type' => 'chars',
+      'trim_suffix' => '',
+      'more_link' => 0,
+      'more_text' => '',
+      'summary_handler' => 'full',
+      'trim_options' => array(),
+    ) + parent::defaultSettings();
+  }
 
-    $settings = $this->getSettings();
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element = parent::settingsForm($form, $form_state);
 
     $element['trim_length'] = array(
       '#title' => t('Trim length'),
       '#type' => 'textfield',
       '#size' => 10,
-      '#default_value' => $settings['trim_length'],
+      '#default_value' => $this->getSetting('trim_length'),
       '#min' => 0,
       '#required' => TRUE,
     );
@@ -58,14 +72,14 @@ class SmartTrimFormatter extends FormatterBase {
         'chars' => t("Characters"),
         'words' => t("Words"),
       ),
-      '#default_value' => $settings['trim_type'],
+      '#default_value' => $this->getSetting('trim_type'),
     );
 
     $element['trim_suffix'] = array(
       '#title' => t('Suffix'),
       '#type' => 'textfield',
       '#size' => 10,
-      '#default_value' => $settings['trim_suffix'],
+      '#default_value' => $this->getSetting('trim_suffix'),
     );
 
     $element['more_link'] = array(
@@ -75,7 +89,7 @@ class SmartTrimFormatter extends FormatterBase {
         0 => t("No"),
         1 => t("Yes"),
       ),
-      '#default_value' => $settings['more_link'],
+      '#default_value' => $this->getSetting('more_link'),
       '#description' => t('Displays a link to the entity (if one exists)'),
     );
 
@@ -83,7 +97,7 @@ class SmartTrimFormatter extends FormatterBase {
       '#title' => t('More link text'),
       '#type' => 'textfield',
       '#size' => 20,
-      '#default_value' => $settings['more_text'],
+      '#default_value' => $this->getSetting('more_text'),
       '#description' => t('If displaying more link, enter the text for the link.'),
     );
 
@@ -96,17 +110,18 @@ class SmartTrimFormatter extends FormatterBase {
           'trim' => t("Use summary if present, honor trim settings"),
           'ignore' => t("Do not use summary"),
         ),
-        '#default_value' => $settings['summary_handler'],
+        '#default_value' => $this->getSetting('summary_handler'),
       );
     }
 
+    $trim_options_value = $this->getSetting('trim_options');
     $element['trim_options'] = array(
       '#title' => t('Additional options'),
       '#type' => 'checkboxes',
       '#options' => array(
         'text' => t('Strip HTML'),
       ),
-      '#default_value' => empty($settings['trim_options']) ? array() : $settings['trim_options'],
+      '#default_value' => empty($trim_options_value) ? array() : $trim_options_value,
     );
 
     return $element;
@@ -117,11 +132,14 @@ class SmartTrimFormatter extends FormatterBase {
    */
   public function settingsSummary() {
     $summary = array();
-    $settings = $this->getSettings();
 
-    $trim_string = $settings['trim_length'] . ' ' . (($settings['trim_type'] == 'chars') ? t('characters') : t('words'));
-    if (drupal_strlen(trim($settings['trim_suffix']))) $trim_string .= " " . t("with suffix");
-    if ($settings['more_link']) $trim_string .= ", " . t("with more link");
+    $trim_string = $this->getSetting('trim_length') . ' ' . (($this->getSetting('trim_type') == 'chars') ? t('characters') : t('words'));
+    if (drupal_strlen(trim($this->getSetting('trim_suffix')))) {
+      $trim_string .= " " . t("with suffix");
+    }
+    if ($this->getSetting('more_link')) {
+      $trim_string .= ", " . t("with more link");
+    }
     $summary[] = $trim_string;
 
     return $summary;
@@ -134,21 +152,23 @@ class SmartTrimFormatter extends FormatterBase {
 
     $element = array();
     $settings = $this->getSettings();
+    $setting_summary_handler = $this->getSetting('summary_handler');
+    $setting_trim_options = $this->getSetting('trim_options');
     $entity = $items->getEntity();
 
     foreach ($items as $delta => $item) {
 
-      if (!empty($settings['summary_handler']) && $settings['summary_handler'] != 'ignore' && !empty($item->summary)) {
+      if (!empty($settings_summary_handler) && $settings_summary_handler != 'ignore' && !empty($item->summary)) {
         $output = $item->summary_processed;
       }
       else {
         $output = $item->processed;
       }
-      $char_delta = drupal_strlen(trim($settings['trim_suffix']));
+      $char_delta = drupal_strlen(trim($this->getSetting('trim_suffix')));
 
       // Process additional options (currently only HTML on/off)
-      if (!empty($settings['trim_options'])) {
-        if (!empty($settings['trim_options']['text'])) {
+      if (!empty($setting_trim_options)) {
+        if (!empty($setting_trim_options['text'])) {
           // Strip tags
           $output = strip_tags(str_replace('<', ' <', $output));
 
@@ -166,13 +186,13 @@ class SmartTrimFormatter extends FormatterBase {
 
       // Make the trim, provided we're not showing a full summary
       $shortened = FALSE;
-      if ($settings['summary_handler'] != 'full' || empty($item->summary)) {
-        if ($settings['trim_type'] == 'words') {
+      if ($this->getSetting('summary_handler') != 'full' || empty($item->summary)) {
+        if ($this->getSetting('trim_type') == 'words') {
           //only bother with this is we have to
-          if ($settings['trim_length'] < str_word_count($output)) {
+          if ($this->getSetting('trim_length') < str_word_count($output)) {
             //use \s or use PREG_CLASS_UNICODE_WORD_BOUNDARY?
             $words = preg_split('/\s/', $output, NULL, PREG_SPLIT_NO_EMPTY);
-            $output2 = implode(" ", array_slice($words, 0,  $settings['trim_length']));
+            $output2 = implode(" ", array_slice($words, 0,  $this->getSetting('trim_length')));
             $output2 = _filter_htmlcorrector($output2);
           }
           //field contained fewer words than we're trimming at, so do nothing
@@ -184,7 +204,7 @@ class SmartTrimFormatter extends FormatterBase {
           //See https://api.drupal.org/api/drupal/core!modules!text!text.module/function/text_summary/8
           //text_summary is smart about looking for paragraphs, sentences,
           //etc, not strictly just length. Uses truncate_utf8 as well
-          $output2 = text_summary($output, $item->format, $settings['trim_length']);
+          $output2 = text_summary($output, $item->format, $this->getSetting('trim_length'));
         }
 
         //verify if we actually performed any shortening
@@ -197,7 +217,7 @@ class SmartTrimFormatter extends FormatterBase {
       // Only include the extension if the text was truncated
       $extension = '';
       if ($shortened) {
-        $extension = $settings['trim_suffix'];
+        $extension = $this->getSetting('trim_suffix');
       }
       // Don't duplicate period at end of text and beginning of extension
       if (substr($output, -1, 1) == '.' && substr($extension, 0, 1) == '.') {
@@ -206,8 +226,8 @@ class SmartTrimFormatter extends FormatterBase {
       // Add the link, if there is one!
       $uri = $entity->uri();
       // But wait! Don't add a more link if the field ends in <!--break-->
-      if ($uri && $settings['more_link'] && strpos(strrev($output), strrev('<!--break-->')) !== 0) {
-        $extension .= l(t($settings['more_text']), $uri['path'], array('html' => TRUE, 'attributes' => array('class' => array('more-link'))));
+      if ($uri && $this->getSetting('more_link') && strpos(strrev($output), strrev('<!--break-->')) !== 0) {
+        $extension .= l(t($this->getSetting('more_text')), $uri['path'], array('html' => TRUE, 'attributes' => array('class' => array('more-link'))));
       }
 
       $output_appended = preg_replace('#^(.*)(\s?)(</[^>]+>)$#Us', '$1' . $extension . '$3', $output);
